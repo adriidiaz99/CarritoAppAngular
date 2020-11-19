04a4b16b68b4e75fa7cdbdceebb37ca587b501f2
Adrián Diáz Santos <adriandiazs1999@gmail.com>
2020-11-19 18:36:09 +0100

    Actualizando a version v1.0

--- CarritoAppAngular/src/app/app.module.ts
+++ CarritoAppAngular/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticulosComponent } from './articulos/articulos.component';
-import { ArticuloComponent } from './articulo/articulo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticulosComponent,
-    ArticuloComponent
+    ArticulosComponent
   ],
   imports: [
     BrowserModule,
--- CarritoAppAngular/src/app/articulos/articulos.component.ts
+++ CarritoAppAngular/src/app/articulos/articulos.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Articulo } from "../../models/Articulo.js";
-import { ArticuloComponent } from '../articulo/articulo.component.js';
 
 @Component({
   selector: 'app-articulos',
@@ -17,10 +16,6 @@ export class ArticulosComponent implements OnInit {
 
   ngOnInit(): void {
     this.listaArticulos.push(new Articulo(1, "Zapato", "/assets/img/zapatos.jpg", "khwcich", true, 12.5, 9.8, "cbehbedhkc", "S", 12));
-  }
-
-  myFunc(){
-    console.log("function called");
   }
 
   eliminarArticulo(articulo:Articulo){
